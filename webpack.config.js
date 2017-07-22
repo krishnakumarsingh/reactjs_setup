3f19c4a82758f83dcf9671344c1be2e9ae2a6d44
Krishna Kumar <kumar.krishna5986@gmail.com>
2017-07-22 14:47:22 -0700

    First setup for ReactJs with CSS.
    Signed-off-by: Krishna Kumar <kumar.krishna5986@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
          },
          {
             test: /\.css$/,
-            loader: 'style!css!sass'
+            loader: 'style-loader!css-loader'
          }
       ]
    },
