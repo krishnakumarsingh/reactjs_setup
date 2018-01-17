b88e5d105893d802888e78af0991a434fab2e95a
krishnakumarsingh <krishnakumarsingh@users.noreply.github.com>
2018-01-17 17:29:31 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
          {
             test: /\.css$/,
             loader: 'style-loader!css-loader'
-         }
+         },
+         { 
+            test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/, 
+            loader: 'url-loader?limit=100000'
+          }
       ]
    },
    plugins: [HtmlWebpackPluginConfig]
