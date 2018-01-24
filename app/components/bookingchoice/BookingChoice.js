5caec1aad4b4b95032a0ce5cccffcc43b52493be
krishnakumarsingh <krishnakumarsingh@users.noreply.github.com>
2018-01-24 16:03:50 +0530

    Update BookingChoice.js

--- app/components/bookingchoice/BookingChoice.js
+++ app/components/bookingchoice/BookingChoice.js
@@ -17,37 +17,13 @@ class BookingChoice extends React.Component{
 		          </div>
 		          <div className="row">
 		            <div className="col-4">
-		              	<div className="card-container">
-				          <div className="card">
-				            <div className="side">
-				              <h4 className="card-title">Card title</h4>
-				              <p className="card-text">To upload your Doctor Prescription.</p>
-				            </div>
-				            <div className="side back">Jimmy Eat World</div>
-				          </div>
-					    </div>
+		              <Cards />
 		            </div>
 		            <div className="col-4">
-		              	<div className="card-container">
-				          <div className="card">
-				            <div className="side">
-				              <h4 className="card-title">Card title</h4>
-				              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				            </div>
-				            <div className="side back">Jimmy Eat World</div>
-				          </div>
-					    </div>
+		              <Cards />
 		            </div>
 		            <div className="col-4">
-		              	<div className="card-container">
-				          <div className="card">
-				            <div className="side">
-				              <h4 className="card-title">Card title</h4>
-				              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				            </div>
-				            <div className="side back">Jimmy Eat World</div>
-				          </div>
-					    </div>
+		              <Cards />
 		            </div>
 		          </div>
 		        </div>
