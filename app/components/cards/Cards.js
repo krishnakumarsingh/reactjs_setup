f225071f756233a053bc221d484fd9b5e66fd933
krishnakumarsingh <krishnakumarsingh@users.noreply.github.com>
2018-01-24 16:02:16 +0530

    Update Cards.js

--- app/components/cards/Cards.js
+++ app/components/cards/Cards.js
@@ -8,15 +8,14 @@ class Cards extends React.Component{
 
 	render(){
     	return(
-    		<div className="card-container">
-          <div className="card">
-            <div className="side">
-              <h4 className="card-title">Card title</h4>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div className="side back">Jimmy Eat World</div>
+        <div className="card tm-card">
+          <img className="card-img-top" src={require('../../images/banner-1.jpg')} width="100%" height="200" alt="Card image cap" />
+          <div className="card-block">
+            <h4 className="card-title">Card title</h4>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button className="btn btn-success"><del className="oldPrice">1,400/-</del> Now <span className="newPrice">800/-</span>Book</button>
           </div>
-		    </div>
+        </div>
     	)
     }
 }
